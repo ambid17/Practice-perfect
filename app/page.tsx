bc3a7a8618bcbce2772d23b498079f59fbbca9a0
Cruzadu <jjake09@gmail.com>
2023-07-01 10:15:28 -0500

    sticky bar

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,87 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <p>Practice Perfect</p>
+      
+      <title>Practice Perfect Webpage</title>
 
-      <h2>your best website solutione team</h2>
+<link rel="stylesheet" href="style.css"></link>
+
+      <div className="center"><h1><a> Practice Perfect</a></h1></div>
+  <br></br>
+    
+      <div className="sticky"><div className="navbar">
+        
+  <a href="#">Library</a>
+    <a href="#">Services</a>
+     <a href="#">About Us</a>
+       <a href="#" className="right">Contact</a>
+   </div> 
+     </div>
+      
+       
+        
+
+<br></br>
+ <h2>your best website solution team</h2>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+      <br></br>
+      <p>Text in here to show up </p>
+
     </main>
   )
 }
