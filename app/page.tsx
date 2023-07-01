3ffcbb8b5e88ee54735e620282438cdbc09e1bfd
Sabien <ambid17@gmail.com>
2023-06-30 20:37:48 -0500

    add project files

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <p>Practice Perfect</p>
+
+      <h2>your best website solution team</h2>
+    </main>
+  )
+}
